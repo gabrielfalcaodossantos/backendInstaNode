c3f2dbc1b308c58bbd833c7f7a2705d85764ac0b
eduqg <eduardoqgomes@gmail.com>
2019-10-02 06:42:13 -0300

    Importando imagem

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const multer = require('multer');
-const routes = new express.Router();
-
+const uploadConfig = require('./config/upload');
 const PostController = require('./controllers/PostController');
-const upload = multer();
+
+const routes = new express.Router();
+const upload = multer(uploadConfig);
 
 routes.post('/posts', upload.single('image'), PostController.store)
 routes.get('/posts', PostController.index)
