b9737fda1cf85393b8bcd47ea8d2991b23db892c
eduqg <eduardoqgomes@gmail.com>
2019-10-02 06:36:56 -0300

    Importando multer para imagens

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -2,14 +2,16 @@ const Post = require('../models/Post');
 
 const mongoose = require('mongoose');
 
-
 module.exports = {
 
     async index(req, res) {
         return res.json({ ok: true });
     },
 
+    // req.body = contêm informações da requisição
+    // req.file = contêm informações do arquivo
     async store(req, res) {
+        console.log(req.body);
         return res.json({ ok: true });
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const multer = require('multer');
 const routes = new express.Router();
 
 const PostController = require('./controllers/PostController');
+const upload = multer();
 
-routes.post('/posts', PostController.store) 
-routes.get('/posts', PostController.index) 
+routes.post('/posts', upload.single('image'), PostController.store)
+routes.get('/posts', PostController.index)
 
 module.exports = routes;
